44d3f49535ff13bdfc5f51052d8c3f05a7f31c91
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2023-02-25 18:16:08 -0500

    Initial commit

---
+++ AlexMemorialWebSite/src/main/java/com/ogulcan/fileManagerDemo/FileManagerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ogulcan.fileManagerDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManagerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileManagerDemoApplication.class, args);
+	}
+
+}
---
+++ AlexMemorialWebSite/src/test/java/com/ogulcan/fileManagerDemo/FileManagerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ogulcan.fileManagerDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileManagerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
